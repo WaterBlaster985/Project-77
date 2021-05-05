3f59c98633d623130ca67adf2241aa76b3b5d488
WaterBlaster985 <74526954+WaterBlaster985@users.noreply.github.com>
2021-05-05 20:47:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Not my first console project, but it's supposed to be")
+var firstname = "Reyansh"
+var lastname = "Reddy"
+var x = 21
+var y = 69
+console.log(firstname.concat(lastname))
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(y%x)
+console.log(2**5)
